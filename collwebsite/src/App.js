1a37d0c838f065a3cd47d924ab0c24f4ccaafee4
Sai Gowtham Tammineni <toney3864@gmail.com>
2024-01-27 07:00:07 +0530

    initial commit

---
+++ collwebsite/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div style={{ marginLeft: '10px', color: 'darkBlue' }} >
+      <img
+        src='asssets/img/logo_t.png'
+        rel='short icon' />
+      <h1>ASSAM WOMEN'S UNIVERSITY</h1>
+    </div >
+  );
+}
+
+export default App;
